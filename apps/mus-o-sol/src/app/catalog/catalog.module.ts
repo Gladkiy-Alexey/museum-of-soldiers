912585fe1b2ff740a5b43c6b67514a45535e7a86
alexeygladkiy <lexxicus85@gmail.com>
2022-05-08 15:45:49 +0300

    (Modules)Replaced component Catalog

--- apps/mus-o-sol/src/app/catalog/catalog.module.ts
+++ apps/mus-o-sol/src/app/catalog/catalog.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { CatalogRoutingModule } from './catalog-routing.module';
-import { CatalogComponent } from './catalog.component';
+import { CatalogComponent } from './catalog/catalog.component';
 
 @NgModule({
   declarations: [CatalogComponent],
