e1e24922c1c3e6d659e5113727d95224ab468b2d
alexeygladkiy <lexxicus85@gmail.com>
2022-05-08 15:51:59 +0300

    (Components)Replaced components

--- apps/mus-o-sol/src/app/museum/museum-routing.module.ts
+++ apps/mus-o-sol/src/app/museum/museum-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { MuseumComponent } from './museum.component';
+import { MuseumComponent } from './museum/museum.component';
 
 const routes: Routes = [{ path: '', component: MuseumComponent }];
 
--- apps/mus-o-sol/src/app/museum/museum.module.ts
+++ apps/mus-o-sol/src/app/museum/museum.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { MuseumRoutingModule } from './museum-routing.module';
-import { MuseumComponent } from './museum.component';
+import { MuseumComponent } from './museum/museum.component';
 
 @NgModule({
   declarations: [MuseumComponent],
