42d4eece3af6c2676e6574134a903fc5bacb44b8
alexeygladkiy <lexxicus85@gmail.com>
2022-05-08 17:12:37 +0300

    (Catalog)Added components

--- apps/mus-o-sol/src/app/catalog/catalog-routing.module.ts
+++ apps/mus-o-sol/src/app/catalog/catalog-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { CatalogComponent } from './catalog/catalog.component';
 
-const routes: Routes = [{ path: '', component: CatalogComponent }];
+import { CatalogComponent } from './catalog/catalog.component';
+import { CategoryComponent } from './category/category.component';
+import { ExhibitComponent } from './exhibit/exhibit.component';
+import { SearchComponent } from './search/search.component';
+
+const routes: Routes = [
+  { path: '', component: CatalogComponent },
+  { path: 'search', component: SearchComponent },
+  { path: 'category/:slug', component: CategoryComponent },
+  { path: 'exhibit/:slug', component: ExhibitComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- apps/mus-o-sol/src/app/catalog/catalog.module.ts
+++ apps/mus-o-sol/src/app/catalog/catalog.module.ts
@@ -3,9 +3,17 @@ import { CommonModule } from '@angular/common';
 
 import { CatalogRoutingModule } from './catalog-routing.module';
 import { CatalogComponent } from './catalog/catalog.component';
+import { CategoryComponent } from './category/category.component';
+import { ExhibitComponent } from './exhibit/exhibit.component';
+import { SearchComponent } from './search/search.component';
 
 @NgModule({
-  declarations: [CatalogComponent],
+  declarations: [
+    CatalogComponent,
+    CategoryComponent,
+    ExhibitComponent,
+    SearchComponent,
+  ],
   imports: [CommonModule, CatalogRoutingModule],
 })
 export class CatalogModule {}
