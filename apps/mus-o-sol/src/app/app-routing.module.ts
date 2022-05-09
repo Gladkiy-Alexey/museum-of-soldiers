6f03c6ef43ae81e0b57f0b3823ba7e16721ed684
alexeygladkiy <lexxicus85@gmail.com>
2022-05-09 20:14:14 +0300

    Changed structure

--- apps/mus-o-sol/src/app/app-routing.module.ts
+++ apps/mus-o-sol/src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { HomeComponent } from './home/home.component';
-import { AboutMeComponent } from './about-me/about-me.component';
-import { AboutCollectionComponent } from './about-collection/about-collection.component';
-import { FeedbackComponent } from './feedback/feedback.component';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutMeComponent } from './pages/about-me/about-me.component';
+import { AboutCollectionComponent } from './pages/about-collection/about-collection.component';
+import { FeedbackComponent } from './pages/feedback/feedback.component';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
 
 const routes: Routes = [
   {
--- apps/mus-o-sol/src/app/app.module.ts
+++ apps/mus-o-sol/src/app/app.module.ts
@@ -6,16 +6,10 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { AboutMeComponent } from './about-me/about-me.component';
-import { AboutCollectionComponent } from './about-collection/about-collection.component';
-import { FeedbackComponent } from './feedback/feedback.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AboutMeComponent,
-    AboutCollectionComponent,
-    FeedbackComponent,
   ],
   imports: [
     AppRoutingModule,
